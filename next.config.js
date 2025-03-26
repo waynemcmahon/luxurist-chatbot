720a3f061803d7706f09511f767dc20a78f91000
waynemcmahon <waynemcmahon22@gmail.com>
2025-03-26 14:28:08 +0000

    Add redirect configuration in next.config.js for enhanced routing
    - Implement a new redirect from all routes (excluding '/embed') to 'https://www.theluxurist.com/magic-quote'
    - Ensure the redirect is permanent for consistent user experience

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/((?!embed).*)',
+        destination: 'https://www.theluxurist.com/magic-quote',
+        permanent: true,
+      },
+    ];
+  },
 };
